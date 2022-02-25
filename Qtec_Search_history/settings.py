076e24c4945ba2f051441aadd81c459b812a4856
jewel <jmbengal.mbstu@gmail.com>
2022-02-26 01:52:51 +0600

    Set up for heroku

--- Qtec_Search_history/settings.py
+++ Qtec_Search_history/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,7 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#start Added by me
+STATICFILES_DIRS = [ #Search during collectstatic
+os.path.join(BASE_DIR,'static'),
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangoAutoStatic') #collectstatic==> automatically created 'djangoStatic' folder.
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
